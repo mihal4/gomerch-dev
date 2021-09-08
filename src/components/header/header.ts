1b8b087ab7020e8b12ab0fa5fae4b4e525ac81d9
Michal <michaldzurilla1@gmail.com>
2021-09-08 09:36:38 +0200

    header layout & dep added

---
+++ src/components/header/header.ts
@@ -0,0 +1,20 @@
+import styled from "styled-components";
+
+export const Header = styled.header`
+  position: absolute;
+  top: 0px;
+  left: 0px;
+  right: 0px;
+  height: 64px;
+  background-color: aqua;
+  padding: 0px 16px;
+  display: flex;
+  -webkit-box-pack: start;
+  justify-content: flex-start;
+  -webkit-box-align: center;
+  align-items: center;
+`;
+
+export const ButtonMenu = styled.button`
+  text-transform: uppercase;
+`;
