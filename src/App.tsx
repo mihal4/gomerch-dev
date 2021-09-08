c3964beb2308399c4b35f17fb5622258094c6831
Michal <michaldzurilla1@gmail.com>
2021-09-08 09:42:32 +0200

    main layout added

--- src/App.tsx
+++ src/App.tsx
@@ -2,6 +2,7 @@ import React from "react";
 import { ButtonMenu, Header } from "./components/header/header";
 import { Container } from "./components/layout/layout";
 import { Nav, Text } from "./components/nav/nav";
+import { Main } from "./components/main/main";
 
 import "./App.css";
 
@@ -14,6 +15,9 @@ function App() {
       <Nav>
         <Text>on</Text>
       </Nav>
+      <Main>
+        <Text>on</Text>
+      </Main>
     </Container>
   );
 }
---
+++ src/components/main/main.ts
@@ -0,0 +1,7 @@
+import styled from "styled-components";
+
+export const Main = styled.main`
+  position: absolute;
+  inset: 64px 0px 0px 200px;
+  background-color: red;
+`;
